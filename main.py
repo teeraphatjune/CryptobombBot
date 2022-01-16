852606ac6119dc33a2eb42f94ebbce58398ed757
teeraphatjune <69952231+teeraphatjune@users.noreply.github.com>
2022-01-16 09:05:20 +0700

    update with threading
    added library threading and rebuild program

--- main.py
+++ main.py
@@ -30,27 +30,33 @@ schedule.every(8).seconds.do(chk_treasure_h)
 def start():
     global running 
     running = True
-    chk_status()
-    processing()
+    # chk_status()
+    # processing()
+    threading.Thread(target=mainfunc).start()
+    txt_status.configure(text = 'Processing', foreground="green")
+    # threading.Thread(target=chk_status).start()
 
 def stop():
     global running 
     running = False
-    chk_status()
+    txt_status.configure(text = 'Stopped', foreground="red")
+    # chk_status()
 
 def mainfunc():
-    login()
-
-def processing():
     if running:
         schedule.run_pending()
-        # threading.Thread(target=chk_status).start()
-        # chk_btn_close()
-        # chk_treasure_h()
         login()
-        # threading.Thread(target=mainfunc).start()
-        gc.collect()
-        root.after(3000, processing)
+        root.after(2000,mainfunc)
+
+# def processing():
+#     threading.Thread(target=mainfunc).start()
+#     # if running:
+#     #     # threading.Thread(target=chk_status).start()
+#     #     # chk_btn_close()
+#     #     # chk_treasure_h()
+#     #     # login()
+#     #     gc.collect()
+#     #     root.after(2000, processing)
 
 def chk_status():
     global running 
@@ -65,11 +71,11 @@ def chk_status():
             txt_status.config(text="Processing{}".format(my_list[counter]), foreground="green")
             counter = 0
             # root.after(1000, chk_status)
-        # txt_status.configure(text = 'Processing', foreground="green")
+        root.after(1000, chk_status)
     else:
         txt_status.configure(text = 'Stopped', foreground="red")
 
-schedule.every(1).seconds.do(chk_status)
+# schedule.every(1).seconds.do(chk_status)
 root = Tk(className='Bot')
 frm = ttk.Frame(root, padding=10)
 frm.grid()
